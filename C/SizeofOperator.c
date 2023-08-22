fcb1a29e762d026b0d47e4a06dcd255bae334c00
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2023-08-22 20:22:37 +0530

    commit

---
+++ C/SizeofOperator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int x=10;
+    short int y=20;
+    long int k=30;
+
+    printf("size of x=%d\n",sizeof(x));
+    printf("size of y=%d\n",sizeof(y));
+    printf("size of k=%d\n",sizeof(k));
+
+
+    return 0;
+}
