7d25076c007153906d76716b87ff04acf3d62fb3
KunalNarkhedePatil <narkhedekunal53@gmail.com>
2023-08-22 23:35:47 +0530

    commit

---
+++ C/Array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int iNo1=10;
+    int iNo2=20;
+    int iNo3=30;
+    int iNo4=40;
+    int iNo5=50;
+
+    int Arr[5]={10,20,30,40,50};
+
+    printf("%d\n",iNo4);
+    printf("%d\n",Arr[3]);
+    return 0;
+}
--- C/SizeofOperator.c
+++ C/SizeofOperator.c
@@ -1,15 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int x=10;
-    short int y=20;
-    long int k=30;
+    char ch = 'A';
+    int i = 21;
+    float f = 12.23;
+    double d = 89.453;
 
-    printf("size of x=%d\n",sizeof(x));
-    printf("size of y=%d\n",sizeof(y));
-    printf("size of k=%d\n",sizeof(k));
+    printf("Size of character is : %d bytes \n", sizeof(ch));
+    printf("Size of integer is : %d bytes \n", sizeof(i));
+    printf("Size of float is : %d bytes \n", sizeof(f));
+    printf("Size of double is : %d bytes \n", sizeof(d));
 
+    printf("Base Address of character is : %d bytes \n", &ch);
+    printf("Base Address of integer is : %d bytes \n", &i);
+    printf("Base Address of float is : %d bytes \n", &f);
+    printf("Base Address of double is : %d bytes \n", &d);
 
     return 0;
 }
